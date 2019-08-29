ceb397cf041e53df955574825eca90b1f03b992b
Skelman50 <skelman50@gmail.com>
2019-08-29 20:58:53 +0300

    some refactoring

--- helpers/validator/index.js
+++ helpers/validator/index.js
@@ -1,31 +1,5 @@
 import expressValidator from "express-validator";
 
-// export const userSignUpValidator = (req, res, next) => {
-//   req
-//     .check("name", "Name is required")
-//     .notEmpty()
-//     .isString()
-//     .withMessage("name must be a string");
-//   req
-//     .check("email", "Email is required")
-//     .notEmpty()
-//     .isEmail()
-//     .withMessage("write correct email");
-//   req
-//     .check("password", "Password is required")
-//     .notEmpty()
-//     .isLength({ min: 6 })
-//     .withMessage("PasswordLength must be min 6 words");
-
-//   const errors = req.validationErrors();
-//   if (errors) {
-//     const error = errors.map(err => err.msg)[0];
-//     return res.status(400).json({ error });
-//   }
-
-//   next();
-// };
-
 const { check, validationResult } = expressValidator;
 
 class Validator {
