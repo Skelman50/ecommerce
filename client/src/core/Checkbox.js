1d7800266149df4c021896efb81d0f3db6240287
Skelman50 <skelman50@gmail.com>
2019-09-12 16:47:10 +0300

    start filters

---
+++ client/src/core/Checkbox.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+
+const Checkbox = ({ categories, handleFilters }) => {
+  const [checked, setChecked] = useState([]);
+
+  const handleToggle = id => () => {
+    const currentCategoryId = checked.indexOf(id);
+    const newCheckedCategoryId = [...checked];
+    if (currentCategoryId === -1) {
+      newCheckedCategoryId.push(id);
+    } else {
+      newCheckedCategoryId.splice(currentCategoryId, 1);
+    }
+
+    handleFilters(newCheckedCategoryId, "category");
+    setChecked(newCheckedCategoryId);
+  };
+  return categories.map((item, idx) => (
+    <li className="list-unstyled" key={idx}>
+      <input
+        onChange={handleToggle(item._id)}
+        value={checked.indexOf(item._id === -1)}
+        type="checkbox"
+        className="form-check-input"
+      />
+      <label className="form-check-label">{item.name}</label>
+    </li>
+  ));
+};
+
+export default Checkbox;
--- client/src/core/Shop.js
+++ client/src/core/Shop.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "./Layout";
+import { apiService } from "../services/api-service";
+import Checkbox from "./Checkbox";
 
 const Shop = () => {
+  const [categories, setCategories] = useState([]);
+  const [error, setError] = useState(false);
+
+  const init = async () => {
+    const response = await apiService.getCategories();
+    if (response.error) {
+      return setError(response.error);
+    }
+    setError(false);
+    setCategories(response);
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  const handleFilters = (filters, filterBy) => {
+    console.log(filters, filterBy);
+  };
+
   return (
     <Layout
       title="Shop page"
@@ -9,7 +31,15 @@ const Shop = () => {
       className="container-fluid"
     >
       <div className="row">
-        <div className="col-4">left side bar</div>
+        <div className="col-4">
+          <h2>Filter by categories</h2>
+          <ul>
+            <Checkbox
+              categories={categories}
+              handleFilters={handleFilters}
+            />
+          </ul>
+        </div>
         <div className="col-8">right</div>
       </div>
     </Layout>
